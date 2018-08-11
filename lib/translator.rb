7c1718ada85e3b40cbf66ec0832d5b82a9423ff3
angelica winkler <caw400@nyu.edu>
2018-08-11 16:49:42 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,10 +6,11 @@ def load_library(file_path)
 
   YAML.load_file(file_path).each do |meaning, array|
     array.each do |key|
-    array[0] = english
-    array[1] = japanese
-    library["translate_to_japanese"][english] = japanese
-    library["translate_to_english"][japanese] = meaning
+      array[0] = english
+      array[1] = japanese
+      library["translate_to_japanese"][english] = japanese
+      library["translate_to_english"][japanese] = meaning
+    end
   end
   library
   binding.pry
