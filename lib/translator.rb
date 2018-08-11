06cf403272eff3513a861a0e7d0e38328206f83d
angelica winkler <caw400@nyu.edu>
2018-08-11 16:45:42 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,17 +2,12 @@ require 'yaml'
 require 'pry'
 
 def load_library(file_path)
-  dictionary = {
-    "get_meaning" => {}, "get_emoticon" => {}
-  }
-  YAML.load_file(file_path).each do |definition, array|
-    array[0] = western_emoticon
-    array[1] = japanese_emoticon
-    dictionary["get_emoticon"][western_emoticon] = japanese_emoticon
-    dictionary["get_meaning"][japanese_emoticon] = western_emoticon
-  end
-  dictionary
-  binding.pry
+  library = {"get_meaning" => {}, "get_emoticon" => {}}
+
+  YAML.load_file(file_path).each do |meaning, array|
+    english, japanese = array
+    library["get_emoticon"][english] = japanese
+    library["get_meaning"][japanese] = meaning
 end
 
 def get_japanese_emoticon
