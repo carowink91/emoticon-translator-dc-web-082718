55a92000b6315ebfee63dee0f204c892f7361f70
angelica winkler <caw400@nyu.edu>
2018-08-11 16:59:54 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,9 +19,8 @@ def get_japanese_emoticon (file_path, emoticon)
   library = load_library(file_path)
   if library["get_emoticon"].include?(emoticon)
     answer = library["get_emoticon"][emoticon]
-    answer
   else
-    puts "Sorry, that emoticon was not found"
+    answer = puts "Sorry, that emoticon was not found"
   end
 
 end
