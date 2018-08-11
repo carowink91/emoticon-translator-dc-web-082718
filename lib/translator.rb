a80224ebb6a2bc2f8a6ceaac6bc591172b6ad4f5
angelica winkler <caw400@nyu.edu>
2018-08-11 16:34:45 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,14 @@
 require 'yaml'
+require 'pry'
 
 def load_library(file_path)
   dictionary = {
     "definition" => {},
     "emoticon" => {}
   }
-  
+  YAML.load_file(file_path).each do |meaning, array|
+
+  end
 end
 
 def get_japanese_emoticon
