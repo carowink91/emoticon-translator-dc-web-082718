9694ec4b5d803f35060a6c2e270a83099a5d6926
angelica winkler <caw400@nyu.edu>
2018-08-11 17:03:05 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,6 +25,9 @@ def get_japanese_emoticon (file_path, emoticon)
   answer
 end
 
-def get_english_meaning
-  # code goes here
+
+
+def get_english_meaning (file_path, emoticon)
+  library = load_library(file_path)
+  binding.pry
 end
